d9a934e13e9677872c46d991b8d320a3fd354729
v-a-l-e-r-i <valeria.dyvak@gmail.com>
2024-07-05 20:06:13 +0300

    added interception of notifications about entering non-numeric characters

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from collections import UserDict
+import re
 
 
 class PhoneTooShortError(Exception):
     pass
+
+
+class NotNumber(Exception):
+    pass
+
+
+def get_phone(func):
+    def inner(self, phone):
+        if len(phone) < 10:
+            raise PhoneTooShortError("Phone number is too short")
+        elif re.findall("\D", phone):
+            raise NotNumber("Need enter a phone number")
+
+        return func(self, phone)
+
+    return inner
 
 
 class Field:
@@ -18,26 +35,21 @@ class Name(Field):
 
 
 class Phone(Field):
-    pass
+    @get_phone
+    def __init__(self, value):
+        super().__init__(value)
 
 
 class Record:
-
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
 
-    def get_phone(func):
-        def inner(self, phone):
-            if len(phone) < 10:
-                raise PhoneTooShortError("Phone number is too short")
-            return func(self, phone)
-
-        return inner
-
-    @get_phone
     def add_phone(self, phone_number):
-        self.phones.append(Phone(phone_number))
+        try:
+            self.phones.append(Phone(phone_number))
+        except Exception as e:
+            print(e)
 
     def remove_phone(self, phone_number):
         self.phones = [phone for phone in self.phones if phone_number != phone.value]
@@ -46,12 +58,12 @@ class Record:
         for phone in self.phones:
             if phone.value == phone_number:
                 phone.value = new_phone_number
-
+            
     def find_phone(self, phone):
         for item in self.phones:
             if item.value == phone:
-                return item.value
-        return "Don`t has this number"
+                return item
+        return None
 
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
@@ -77,11 +89,8 @@ book = AddressBook()
 
 # Створення запису для John
 john_record = Record("John")
-try:
-    john_record.add_phone("1234567890")
-    john_record.add_phone("555555555")
-except PhoneTooShortError as e:
-    print(e)
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
 
 # Додавання запису John до адресної книги
 book.add_record(john_record)
@@ -102,7 +111,7 @@ john.edit_phone("1234567890", "1112223333")
 print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
 
 # Пошук конкретного телефону у записі John
-found_phone = john.find_phone("1234567890")
+found_phone = john.find_phone("1212223333")
 print(f"{john.name}: {found_phone}")  # Виведення: John: 5555555555
 
 # Видалення запису Jane
